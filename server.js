c369489ce69b386ee6a2c1f09ab31d52939e75cc
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2025-11-09 13:47:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,9 @@
-// Import the Express module
 const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// Serve static files (like HTML, CSS, JS)
 app.use(express.static('public'));
 
-// Define a simple route
-app.get('/hello', (req, res) => {
-  res.send('Hello from Node.js!');
-});
-
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
-
